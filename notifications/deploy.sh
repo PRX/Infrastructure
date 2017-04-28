7851c97c223a65f68040175e3068485b0fd8561b
Chris Kalafarski <chris@farski.com>
2017-04-28 18:26:06 -0400

    Clean up Lambda code S3 scripts

--- notifications/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-mkdir -p .deploy
-cd lambdas; find * -maxdepth 0 -type d|while read dirname; do cd "$dirname"; zip -r "$dirname" *; mv "$dirname".zip ../../.deploy; cd ..; done; cd ..
-
-aws s3 sync .deploy/ s3://prx-infrastructure-code-us-east-2/notifications --acl private --region us-east-2
