836ff751fe9e1f069cffb1bd18f82f4f453c4d87
Chris Kalafarski <chris@farski.com>
2017-05-08 10:30:13 -0400

    Work on state capture

--- cd/lambdas/environment-state-capture/index.js
+++ cd/lambdas/environment-state-capture/index.js
@@ -10,6 +10,18 @@ exports.handler = (event, context, callback) => {
     try {
         console.log('Starting capture...');
 
+        const artifacts = job.data.InputArtifacts;
+
+        const repo = artifacts.find(e => e.name.test(/RepoSource/));
+        const config = artifacts.find(e => e.name.test(/TemplateConfig/));
+
+        const capture = JSON.stringify({
+          TemplateConfigArchiveS3Version: config.revision,
+          InfrastructureGitCommit: repo.revision
+        });
+
+        console.log(capture);
+
         codepipeline.putJobSuccessResult({ jobId: job.id }, (e, d) => {
             callback(null, '...Done!');
         });
