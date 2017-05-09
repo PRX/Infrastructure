e2d5b106ae5d94d343c1cd565fbf023119e10b2a
Chris Kalafarski <chris@farski.com>
2017-05-09 00:30:58 -0400

    Split up housekeeping CD functions

--- cd/lambdas/infrastructure-s3-sync/lambda_function.py
+++ cd/lambdas/infrastructure-s3-sync/lambda_function.py
@@ -8,10 +8,6 @@
 # object prefix of the Git commit hash.
 # TODO currently this is using `latest` as the prefix
 #
-# This function also creates an output artifact, which is a zipped JSON file
-# that contains the Git commit hash of the input repo artifact. The destination
-# bucket for that is the native CodePipeline artifact store bucket.
-#
 # This should always callback to the CodePipeline API to indicate success or
 # failure.
 
@@ -36,32 +32,6 @@ def put_job_failure(job, message):
     print('Putting job failure')
     print(message)
     code_pipeline.put_job_failure_result(jobId=job['id'], failureDetails={'message': message, 'type': 'JobFailed'})
-
-# Gets the revision value (Git sha) of the input artifact, and puts it in a
-# JSON file that gets zipped and sent to S3 as an output artifact
-def publish_revision(job):
-    input_artifact = job['data']['inputArtifacts'][0]
-    output_artifact = job['data']['outputArtifacts'][0]
-
-    sha = input_artifact['revision']
-
-    print(f"...Publishing revision {sha}...")
-
-    body = json.dumps({ 'commit': sha });
-
-    archive_path = "/tmp/{0}".format(uuid.uuid4())
-
-    # TODO Should be able to do this all in memory
-    archive = zipfile.ZipFile(archive_path, mode='w')
-    archive.writestr('state.json', body, compress_type=zipfile.ZIP_DEFLATED)
-    archive.close()
-
-    output_location = output_artifact['location']['s3Location']
-    output_bucket = output_location['bucketName']
-    output_key = output_location['objectKey']
-    s3.upload_file(archive_path, output_bucket, output_key)
-
-    print(f"...Wrote to {output_bucket}/{output_key}...")
 
 def sync_source(job):
     print(f"...Syncing repository source...")
@@ -104,7 +74,6 @@ def lambda_handler(event, context):
 
         job = event['CodePipeline.job']
 
-        publish_revision(job)
         sync_source(job)
 
         put_job_success(job, '')
