4b274639469f71221c1db5bf923ad8f558e02ea0
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-05-24 17:35:58 -0400

    move secrets to own stack

--- setup.sh
+++ setup.sh
@@ -12,4 +12,7 @@ cd ./ci/lambdas; find * -maxdepth 0 -type d|while read dirname; do cd "$dirname"
 mkdir -p .deploy/cd
 cd ./cd/lambdas; find * -maxdepth 0 -type d|while read dirname; do cd "$dirname"; zip -r "$dirname" *; mv "$dirname".zip ../../../.deploy/cd; cd ..; done; cd ..; cd ..
 
+mkdir -p .deploy/secrets
+cd ./secrets/lambdas; find * -maxdepth 0 -type d|while read dirname; do cd "$dirname"; zip -r "$dirname" *; mv "$dirname".zip ../../../.deploy/secrets; cd ..; done; cd ..; cd ..
+
 aws s3 sync .deploy/ s3://prx-infrastructure-us-east-1-support/ --acl private --region us-east-1
