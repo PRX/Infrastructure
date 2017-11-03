f8da50f87be2b51b12b4b8de7a9fe56ff9b170b0
Chris Kalafarski <chris@farski.com>
2017-11-03 16:16:19 -0400

    Add basic template continuity tester

---
+++ utility/continuity.py
@@ -0,0 +1,28 @@
+import boto3
+
+client = boto3.client('cloudformation')
+
+map = {
+    'cd': ['infrastructure-cd', '../ci/ci.yml'],
+    'ci': ['infrastructure-ci', '../cd/cd.yaml'],
+    'notifications': ['infrastructure-notifications', '../notifications/notifications.yml'],
+    'secrets': ['infrastructure-secrets', '../secrets/secrets.yml'],
+    'storage': ['infrastructure-storage', '../storage/storage.yml'],
+}
+
+for key in map:
+    response = client.get_template(StackName=map[key][0])
+    remote = response['TemplateBody']
+
+    local = open(map[key][1], "r").read()
+
+    if not local == remote:
+        print(f"mismatch {key}")
+
+
+
+
+
+
+
+
