9d0fafc8161fccb14397987297e405e4e4b91154
Chris Kalafarski <chris@farski.com>
2017-11-03 22:40:55 -0400

    Fix acceptance project policy

--- cd/lambdas/pipeline-fail-notification/lambda_function.py
+++ cd/lambdas/pipeline-fail-notification/lambda_function.py
@@ -43,6 +43,7 @@ def slack_message(action_state):
 # Builds a fully-formed Slack message and publishes it to the Slack Message
 # Relay topic to get forwarded to Slack
 def publish_slack_message(action_state):
+    print('...Publishing message...')
     sns.publish(
         TopicArn=os.environ['SLACK_MESSAGE_RELAY_TOPIC_ARN'],
         Message=json.dumps(slack_message(action_state))
@@ -64,6 +65,7 @@ def lambda_handler(event, context):
 
         # Get the state history of the pipeline
         pipeline_state = code_pipeline.get_pipeline_state(name=pipeline_name)
+        print('...Got pipeline state...')
 
         # Look through the history and see if there have been any changes in
         # the last 60 seconds (ie since the last time the function executed).
@@ -71,13 +73,17 @@ def lambda_handler(event, context):
         for stage_state in pipeline_state['stageStates']:
             for action_state in stage_state['actionStates']:
                 if 'latestExecution' in action_state:
+                    print('...Checking latest execution...')
                     execution = action_state['latestExecution']
 
                     tz = execution['lastStatusChange'].tzinfo
                     period_start = datetime.now(tz) - timedelta(seconds=60)
 
                     if execution['lastStatusChange'] > period_start:
+                        print('...Found recent execution...')
+
                         if execution['status'] == 'Failed':
+                            print('...Found recent failure...')
                             publish_slack_message(action_state)
 
         return '...Done'
