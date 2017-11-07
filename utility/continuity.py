38aea3ba11cbdf6182b792f0435d75083c039055
Chris Kalafarski <chris@farski.com>
2017-11-07 15:28:11 -0500

    Add basic template continuity test script

--- utility/continuity.py
+++ utility/continuity.py
@@ -2,22 +2,27 @@ import boto3
 
 client = boto3.client('cloudformation')
 
-map = {
-    'cd': ['infrastructure-cd', '../ci/ci.yml'],
-    'ci': ['infrastructure-ci', '../cd/cd.yaml'],
-    'notifications': ['infrastructure-notifications', '../notifications/notifications.yml'],
-    'secrets': ['infrastructure-secrets', '../secrets/secrets.yml'],
-    'storage': ['infrastructure-storage', '../storage/storage.yml'],
-}
+response = client.describe_stacks()
+for stack in response['Stacks']:
+    print(f"{stack['StackName']} {stack['NotificationARNs']}")
 
-for key in map:
-    response = client.get_template(StackName=map[key][0])
-    remote = response['TemplateBody']
 
-    local = open(map[key][1], "r").read()
+# map = {
+#     'cd': ['infrastructure-cd', '../ci/ci.yml'],
+#     'ci': ['infrastructure-ci', '../cd/cd.yaml'],
+#     'notifications': ['infrastructure-notifications', '../notifications/notifications.yml'],
+#     'secrets': ['infrastructure-secrets', '../secrets/secrets.yml'],
+#     'storage': ['infrastructure-storage', '../storage/storage.yml'],
+# }
 
-    if not local == remote:
-        print(f"mismatch {key}")
+# for key in map:
+#     response = client.get_template(StackName=map[key][0])
+#     remote = response['TemplateBody']
+
+#     local = open(map[key][1], "r").read()
+
+#     if not local == remote:
+#         print(f"mismatch {key}")
 
 
 
