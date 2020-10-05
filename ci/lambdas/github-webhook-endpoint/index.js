22435f77efe5e4fb0c666e0266dfb39940f7ab7b
Chris Kalafarski <chris@farski.com>
2020-10-05 13:06:55 -0400

    Modernize GitHub endpoint Lambda code
    Closes #430

--- ci/lambdas/github-webhook-endpoint/index.js
+++ ci/lambdas/github-webhook-endpoint/index.js
@@ -12,14 +12,14 @@ const OK_RESPONSE = { statusCode: 200, headers: {}, body: null };
 
 const sns = new AWS.SNS({ apiVersion: '2010-03-31' });
 
-function publishEvent(event, callback) {
-    console.log('...Publishing event to SNS...');
+function publishEvent(event) {
+    console.log('Publishing event to SNS');
 
-    const body = event.isBase64Encoded ? Buffer.from(event.body, 'base64').toString('utf-8') : event.body;
-
-    sns.publish({
+    return sns.publish({
         TopicArn: process.env.GITHUB_EVENT_HANDLER_TOPIC_ARN,
-        Message: body,
+        Message: event.isBase64Encoded
+            ? Buffer.from(event.body, 'base64').toString('utf-8')
+            : event.body,
         MessageAttributes: {
             githubEvent: {
                 DataType: 'String',
@@ -30,53 +30,31 @@ function publishEvent(event, callback) {
                 StringValue: event.headers['x-github-delivery'],
             },
         },
-    }, (err) => {
-        if (err) {
-            console.error('...Publishing event failed!');
-            callback(err);
-        } else {
-            console.log('...Event published!');
-            callback(null, OK_RESPONSE);
-        }
-    });
+    }).promise();
 }
 
-function handleEvent(event, callback) {
-    console.log(`...Handling event: ${event.headers['x-github-event']}...`);
-    switch (event.headers['x-github-event']) {
-        case 'ping':
-            // Blackhole `ping` events
-            callback(null, OK_RESPONSE);
-            break;
-        default:
-            publishEvent(event, callback);
-    }
-}
-
-function main(event, context, callback) {
+exports.handler = async (event) => {
     const githubSignature = event.headers['x-hub-signature'].split('=')[1];
 
-    console.log(`Checking event signature: ${githubSignature}...`);
+    console.log(`Checking event signature: ${githubSignature}`);
 
     const key = process.env.GITHUB_WEBHOOK_SECRET;
     const data = event.body;
     const check = crypto.createHmac('sha1', key).update(data).digest('hex');
 
-    if (githubSignature === check) {
-        handleEvent(event, callback);
-    } else {
-        // Raising an error here so it's visible if we start getting requests
-        // that aren't valid.
-        console.error('...Request signature was invalid!');
-        callback(new Error('Invalid signature!'));
+    if (githubSignature !== check) {
+        throw new Error('Invalid signature!');
     }
-}
 
-exports.handler = (event, context, callback) => {
-    try {
-        main(event, context, callback);
-    } catch (e) {
-        console.error('Unhandled exception!');
-        callback(e);
+    console.log(`Handling event: ${event.headers['x-github-event']}`);
+
+    switch (event.headers['x-github-event']) {
+        case 'ping':
+            // Blackhole `ping` events
+            break;
+        default:
+            await publishEvent(event);
     }
+
+    return OK_RESPONSE;
 };
