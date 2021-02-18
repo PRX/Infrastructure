f7e4165ef3299bb3fdfba44b3da5382d64559a6c
Chris Kalafarski <chris@farski.com>
2021-02-18 15:05:05 -0500

    Format serverless S3 Python code

--- etc/serverless-s3-upload/lambdas/WebsiteApiFunction/lambda_function.py
+++ etc/serverless-s3-upload/lambdas/WebsiteApiFunction/lambda_function.py
@@ -14,7 +14,7 @@ import hashlib
 import hmac
 from datetime import datetime, timedelta
 
-AMZ_ALGORITHM = 'AWS4-HMAC-SHA256'
+AMZ_ALGORITHM = "AWS4-HMAC-SHA256"
 
 
 # Returns information about the values used to derive the signing key used to
@@ -23,9 +23,9 @@ AMZ_ALGORITHM = 'AWS4-HMAC-SHA256'
 # <your-access-key-id>/<date>/<aws-region>/<aws-service>/aws4_request
 # eg: AKIAIOSFODNN7EXAMPLE/20130728/us-east-1/s3/aws4_request
 def signing_credentials(signing_time):
-    access_key_id = os.environ['UPLOAD_ACCESS_KEY_ID']
-    date_stamp = signing_time.strftime('%Y%m%d')
-    region = os.environ['AWS_REGION']
+    access_key_id = os.environ["UPLOAD_ACCESS_KEY_ID"]
+    date_stamp = signing_time.strftime("%Y%m%d")
+    region = os.environ["AWS_REGION"]
 
     return f"{access_key_id}/{date_stamp}/{region}/s3/aws4_request"
 
@@ -38,50 +38,52 @@ def s3_post_policy(signing_time, ttl=60):
     print(f"== EXPIRES == {expiration_date.strftime('%Y-%m-%dT%H:%M:%SZ')}")
 
     return {
-        'expiration': expiration_date.strftime('%Y-%m-%dT%H:%M:%SZ'),
-        'conditions': [
-            {'bucket': os.environ['BUCKET']},
-
-            {'x-amz-algorithm': AMZ_ALGORITHM},
-            {'x-amz-credential': signing_credentials(signing_time)},
-            {'x-amz-date': signing_time.strftime('%Y%m%dT%H%M%SZ')},
-
+        "expiration": expiration_date.strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "conditions": [
+            {"bucket": os.environ["BUCKET"]},
+            {"x-amz-algorithm": AMZ_ALGORITHM},
+            {"x-amz-credential": signing_credentials(signing_time)},
+            {"x-amz-date": signing_time.strftime("%Y%m%dT%H%M%SZ")},
             ["starts-with", "$success_action_redirect", ""],
             ["starts-with", "$key", "upload/"],
-            ["starts-with", "$Content-Type", "audio/"]
-        ]
+            ["starts-with", "$Content-Type", "audio/"],
+        ],
     }
 
 
 # Returns a HMAC-SHA256 hash of the given message, using the given key
 def digest(key, msg):
-    return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+    return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
 
 
 # Derives a signing key for an AWS V4 signature
-# See step 1: https://docs.aws.amazon.com/general/latest/gr/sigv4-calculate-signature.html
+# See step 1:
+# https://docs.aws.amazon.com/general/latest/gr/sigv4-calculate-signature.html
 def aws_v4_signing_key(access_key, date_stamp, region, service):
-    date_key = digest(('AWS4' + access_key).encode('utf-8'), date_stamp)
+    date_key = digest(("AWS4" + access_key).encode("utf-8"), date_stamp)
     date_region_key = digest(date_key, region)
     date_region_service_key = digest(date_region_key, service)
 
-    signing_key = digest(date_region_service_key, 'aws4_request')
+    signing_key = digest(date_region_service_key, "aws4_request")
 
     return signing_key
 
 
 # Returns an AWS V4 signature for the given string. Generates a signing key
 # for S3 in the Lambda execution region
-# See step 2: https://docs.aws.amazon.com/general/latest/gr/sigv4-calculate-signature.html
+# See step 2:
+# https://docs.aws.amazon.com/general/latest/gr/sigv4-calculate-signature.html
 def aws_v4_signature(signing_time, string_to_sign):
-    access_key = os.environ['UPLOAD_SECRET_ACCESS_KEY']
-    date_stamp = signing_time.strftime('%Y%m%d')
-    region = os.environ['AWS_REGION']
-    service = 's3'
+    access_key = os.environ["UPLOAD_SECRET_ACCESS_KEY"]
+    date_stamp = signing_time.strftime("%Y%m%d")
+    region = os.environ["AWS_REGION"]
+    service = "s3"
 
     signing_key = aws_v4_signing_key(access_key, date_stamp, region, service)
 
-    signature = hmac.new(signing_key, string_to_sign.encode('utf-8'), hashlib.sha256).hexdigest()
+    signature = hmac.new(
+        signing_key, string_to_sign.encode("utf-8"), hashlib.sha256
+    ).hexdigest()
 
     return signature
 
@@ -95,24 +97,28 @@ def lambda_handler(event, context):
     post_policy = s3_post_policy(signing_time)
 
     post_policy_json = json.dumps(post_policy)
-    post_policy_json_b64 = base64.b64encode(post_policy_json.encode('utf-8')).decode('utf-8')
+    post_policy_json_b64 = base64.b64encode(post_policy_json.encode("utf-8")).decode(
+        "utf-8"
+    )
 
     # An AWS V4 signature of the POST policy
     policy_signature = aws_v4_signature(signing_time, post_policy_json_b64)
 
     return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Methods': 'GET',
-            'Access-Control-Allow-Origin': '*'
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Origin": "*",
         },
-        'body': json.dumps({
-            'amz_algorithm': AMZ_ALGORITHM,
-            'amz_credential': signing_credentials(signing_time),
-            'amz_date': signing_time.strftime('%Y%m%dT%H%M%SZ'),
-            'base64_policy': post_policy_json_b64,
-            'bucket_domain_name': os.environ['BUCKET_DOMAIN_NAME'],
-            'public_access_host': os.environ['PUBLIC_ACCESS_HOST'],
-            'signature': policy_signature
-        })
+        "body": json.dumps(
+            {
+                "amz_algorithm": AMZ_ALGORITHM,
+                "amz_credential": signing_credentials(signing_time),
+                "amz_date": signing_time.strftime("%Y%m%dT%H%M%SZ"),
+                "base64_policy": post_policy_json_b64,
+                "bucket_domain_name": os.environ["BUCKET_DOMAIN_NAME"],
+                "public_access_host": os.environ["PUBLIC_ACCESS_HOST"],
+                "signature": policy_signature,
+            }
+        ),
     }
