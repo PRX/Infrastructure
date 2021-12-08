052c2cfab79d6cac3c6b792d840212b45dc94ed5
Chris Kalafarski <chris@farski.com>
2021-12-08 07:57:14 -0500

    Turn on more channels

--- devops/chat-ops/cloudwatch-alarms/src/slack/channels.js
+++ devops/chat-ops/cloudwatch-alarms/src/slack/channels.js
@@ -14,18 +14,15 @@ module.exports = {
       return '#ops-error';
     } else if (name.startsWith('WARN')) {
       return '#ops-warn';
+    } else if (name.startsWith('INFO')) {
+      return '#ops-info';
+    } else if (name.startsWith('CRITICAL')) {
+      return '#ops-fatal';
+    } else if (name.startsWith('MAJOR')) {
+      return '#ops-error';
+    } else if (name.startsWith('MINOR')) {
+      return '#ops-warn';
     }
-    // } else if (name.startsWith('WARN')) {
-    //   return '#ops-warn';
-    // } else if (name.startsWith('INFO')) {
-    //   return '#ops-info';
-    // } else if (name.startsWith('CRITICAL')) {
-    //   return '#ops-info';
-    // } else if (name.startsWith('MAJOR')) {
-    //   return '#ops-info';
-    // } else if (name.startsWith('MINOR')) {
-    //   return '#ops-info';
-    // }
 
     return '#sandbox2';
   },
