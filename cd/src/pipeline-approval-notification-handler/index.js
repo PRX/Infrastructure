1a1b90c075f15d2b7f20709951f1beb53bc82504
Chris Kalafarski <chris@farski.com>
2022-06-09 16:23:25 -0400

    Handle SSM values in change set deltas

--- cd/src/pipeline-approval-notification-handler/index.js
+++ cd/src/pipeline-approval-notification-handler/index.js
@@ -70,14 +70,15 @@ function unchangedParameters(stackParameters, changeSetParameters) {
     return changeSetParameters.find(
       (s) =>
         s.ParameterKey === p.ParameterKey &&
-        s.ParameterValue === p.ParameterValue,
+        (s.ParameterValue || s.ResolvedValue) ===
+          (p.ParameterValue || s.ResolvedValue),
     );
   });
 }
 
 /**
- * Returns all unique parameters present in either the stack parameters or the
- * change set parameters
+ * Returns an array all unique parameters keys (strings) present in either the
+ * stack parameters or the change set parameters
  * @param {AWS.CloudFormation.Parameters} stackParameters
  * @param {AWS.CloudFormation.Parameters} changeSetParameters
  * @returns {AWS.CloudFormation.ParameterKey[]}
@@ -95,19 +96,32 @@ function allParameterKeys(stackParameters, changeSetParameters) {
  * Returns the parameter deltas comparing the stack and change set parameters
  * in the form [parameter key, stack value, change set value]. If the parameter
  * is missing from the stack or change set, the value will be undefined.
+ *
+ * When the value for the stack and change set are the same the parameter is
+ * excluded.
+ *
+ * The resolved value for a parameter will be used if present, so for SSM-based
+ * parameters, the delta is based on the value in Parameter Store, not the
+ * actual parameter value.
  * @param {AWS.CloudFormation.Parameters} stackParameters
  * @param {AWS.CloudFormation.Parameters} changeSetParameters
  * @returns {ParameterDeltas}
  */
 function parameterDeltas(stackParameters, changeSetParameters) {
   return allParameterKeys(stackParameters, changeSetParameters)
-    .map((k) => {
+    .map((key) => {
+      const stackParam = stackParameters.find((p) => p.ParameterKey === key);
+      const stackParamValue =
+        stackParam?.ResolvedValue || stackParam?.ParameterValue;
+
+      const changeSetParam = changeSetParameters.find(
+        (p) => p.ParameterKey === key,
+      );
+      const changeSetParamValue =
+        changeSetParam?.ResolvedValue || changeSetParam?.ParameterValue;
+
       /** @type {ParameterDelta} */
-      const delta = [
-        k,
-        stackParameters.find((p) => p.ParameterKey === k)?.ParameterValue,
-        changeSetParameters.find((p) => p.ParameterKey === k)?.ParameterValue,
-      ];
+      const delta = [key, stackParamValue, changeSetParamValue];
       return delta;
     })
     .filter((d) => d[1] !== d[2]);
@@ -115,7 +129,15 @@ function parameterDeltas(stackParameters, changeSetParameters) {
 
 /**
  * Builds a Slack markdown flavored string representing a specific parameter
- * value, based on the type of parameter (Git commit, Docker tag, etc)
+ * value, based on the form of parameter value. It will contain a link to the
+ * relevant commit in GitHub, so the repo and commit ID are extracted from the
+ * value.
+ *
+ * Some values are Docker image tags like:
+ * 111111111111.dkr.ecr.us-east-1.amazonaws.com/github/prx/my-repo:f4a8a88f7adafa4d2f268573672374af025a314
+ *
+ * Some values are S3 object names that contain GitHub info:
+ * GitHub/PRX/my-repo/f4a8a88f7adafa4d2f268573672374af025a314.zip
  * @param {AWS.CloudFormation.ParameterKey} key
  * @param {AWS.CloudFormation.ParameterValue} value
  * @param {Boolean} noLinks
@@ -130,11 +152,14 @@ function parameterDeltasListValue(key, value, noLinks = false) {
     return `\`${value}\``;
   }
 
+  // The InfrastructureGitCommit parameter value is a just a commit hash,
+  // so it is handled explicitly
   if (key === 'InfrastructureGitCommit') {
     const url = `https://github.com/PRX/Infrastructure/commit/${value}`;
     return `\`<${url}|${value.slice(0, 7)}>\``;
   }
 
+  // Look for anything containing "dkr.ecr", which is an ECR Docker image tag
   if (/dkr\.ecr/.test(value)) {
     const repo = value.match(/github\/([^:]+):/)[1];
     const commit = value.match(/:([0-9a-f]{40})$/)[1];
@@ -156,8 +181,19 @@ function parameterDeltasListValue(key, value, noLinks = false) {
 }
 
 /**
- * Returns an arrow emoji for use when listed parameter changes. The arrow may
- * behave differently depending on the type of parameter it is representing
+ * Returns an arrow emoji that is shown when displaying a parameter value that
+ * has changed. The arrow will be a link to a diff in GitHub for the before and
+ * after commits. The repo and commit IDs are extract from the parameter values
+ * based on the form of the value.
+ *
+ * This is JUST the arrow, it doesn't affect what is arround the arrow in the
+ * message, even though it deals with those values.
+ *
+ * Some values are Docker image tags like:
+ * 111111111111.dkr.ecr.us-east-1.amazonaws.com/github/prx/my-repo:f4a8a88f7adafa4d2f268573672374af025a314
+ *
+ * Some values are S3 object names that contain GitHub info:
+ * GitHub/PRX/my-repo/f4a8a88f7adafa4d2f268573672374af025a314.zip
  * @param {ParameterDelta} parameterDelta
  * @param {Boolean} noLinks
  * @returns {String}
@@ -167,34 +203,46 @@ function parameterDeltasListArrow(parameterDelta, noLinks = false) {
     return '➡';
   }
 
-  if (parameterDelta[0] === 'InfrastructureGitCommit') {
-    const url = `https://github.com/PRX/Infrastructure/compare/${parameterDelta[1]}...${parameterDelta[2]}`;
+  const parameterKey = parameterDelta[0];
+  const beforeValue = parameterDelta[1];
+  const afterValue = parameterDelta[2];
+
+  // The InfrastructureGitCommit parameter value is a just a commit hash,
+  // so it is handled explicitly
+  if (parameterKey === 'InfrastructureGitCommit') {
+    const url = `https://github.com/PRX/Infrastructure/compare/${beforeValue}...${afterValue}`;
     return `<${url}|➡>`;
   }
 
+  // Look for anything containing "dkr.ecr", which is an ECR Docker image tag
+  // TODO This doesn't check if the repo in before and after changed
   if (
-    /EcrImageTag/.test(parameterDelta[0]) &&
-    parameterDelta[0] &&
-    parameterDelta[1] &&
-    parameterDelta[2]
+    beforeValue &&
+    afterValue &&
+    /dkr\.ecr/.test(beforeValue) &&
+    /dkr\.ecr/.test(afterValue)
   ) {
-    const slug = parameterDelta[0].replace('EcrImageTag', '');
-    const sha1 = parameterDelta[1].split(':')[1];
-    const sha2 = parameterDelta[2].split(':')[1];
-    const url = `https://github.com/PRX/${slug}.prx.org/compare/${sha1}...${sha2}`;
+    const repo = afterValue.match(/github\/([^:]+):/)[1];
+
+    const oldCommit = beforeValue.match(/:([0-9a-f]{40})$/)[1];
+    const newCommit = afterValue.match(/:([0-9a-f]{40})$/)[1];
+
+    const url = `https://github.com/${repo}/compare/${oldCommit}...${newCommit}`;
     return `<${url}|➡>`;
   }
 
   // Look for `GitHub/[CHARS]/[CHARS]/[HEX HASH]`
+  // TODO This doesn't check if the repo in before and after changed
   if (
-    parameterDelta[1] &&
-    parameterDelta[2] &&
-    /GitHub\/[^\/]+\/[^\/]+\/[a-f0-9]{40}/.test(parameterDelta[2])
+    beforeValue &&
+    afterValue &&
+    /GitHub\/[^\/]+\/[^\/]+\/[a-f0-9]{40}/.test(beforeValue) &&
+    /GitHub\/[^\/]+\/[^\/]+\/[a-f0-9]{40}/.test(afterValue)
   ) {
-    const repo = parameterDelta[2].match(/GitHub\/([^\/]+\/[^\/]+)/)[1];
+    const repo = afterValue.match(/GitHub\/([^\/]+\/[^\/]+)/)[1];
 
-    const oldCommit = parameterDelta[1].match(/\/([0-9a-f]{40})/)[1];
-    const newCommit = parameterDelta[2].match(/\/([0-9a-f]{40})/)[1];
+    const oldCommit = beforeValue.match(/\/([0-9a-f]{40})/)[1];
+    const newCommit = afterValue.match(/\/([0-9a-f]{40})/)[1];
 
     const url = `https://github.com/${repo}/compare/${oldCommit}...${newCommit}`;
     return `<${url}|➡>`;
