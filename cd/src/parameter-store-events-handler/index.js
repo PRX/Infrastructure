9e41ba5eeb13a9ff798e8bcd8df4ec24aaabe76d
Chris Kalafarski <chris@farski.com>
2022-07-18 09:40:14 -0400

    Update parameter events function

--- cd/src/parameter-store-events-handler/index.js
+++ cd/src/parameter-store-events-handler/index.js
@@ -1,4 +1,24 @@
+const AWS = require('aws-sdk');
+
+const codepipeline = new AWS.CodePipeline({ apiVersion: '2015-07-09' });
+
 // https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-paramstore-cwe.html
+/**
+ * This function is subscribed to some EventBridge events related to parameters
+ * in Paramater Store. It is intended to start Spire CD pipeline executions
+ * when relevant events take place, such as a staging application Docker tag
+ * getting updated.
+ * @param {*} event
+ */
 exports.handler = async (event) => {
   console.log(JSON.stringify(event));
+
+  if (
+    event.detail.name.startsWith('/prx/stag/Spire/') ||
+    event.detail.name.startsWith('/prx/global/Spire/')
+  ) {
+    await codepipeline
+      .startPipelineExecution({ name: process.env.PIPELINE_NAME })
+      .promise();
+  }
 };
