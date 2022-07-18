682179c85efb63b9e0cb0adf3ba7270f860b7039
Chris Kalafarski <chris@farski.com>
2022-07-18 18:52:56 -0400

    Add new ECR image push script

--- ci/utility/post_build.sh
+++ ci/utility/post_build.sh
@@ -2,8 +2,59 @@
 set -e
 set -a
 
-# Look for any Docker images labeled with "org.prx.app"
+# Look for any Docker images labeled with "org.prx.spire.publish.ecr"
 push_to_ecr() {
+    echo ">>> Looking for publishable Docker images"
+    image_ids=$(docker images --filter "label=org.prx.spire.publish.ecr" --format "{{.ID}}")
+
+    if [ -z "$image_ids" ]; then
+        echo "< No Docker images found. Set the org.prx.spire.publish.ecr LABEL in a Dockerfile to publish its image."
+    else
+        for image_id in $image_ids; do
+            echo "> Publishing Docker image: $image_id..."
+
+            label=$(docker inspect --format '{{ index .Config.Labels "org.prx.spire.publish.ecr"}}' "$image_id")
+
+            echo "> Logging into ECR"
+            $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
+            echo "> Logged in to ECR"
+
+            unsafe_ecr_repo_name="GitHub/${PRX_REPO}"
+            # Do any transformations necessary to satisfy ECR naming requirements:
+            # Start with letter, [a-z0-9-_/.] (maybe, docs are unclear)
+            safe_ecr_repo_name=$(echo "$unsafe_ecr_repo_name" | tr '[:upper:]' '[:lower:]')
+
+            # Need to allow errors temporarily to check if the repo exists
+            set +e
+            aws ecr describe-repositories --repository-names "$safe_ecr_repo_name" > /dev/null 2>&1
+            if [ $? -eq 0 ]
+            then
+                echo "> ECR Repository already exists"
+            else
+                echo "> Creating ECR repository"
+                aws ecr create-repository --repository-name "$safe_ecr_repo_name"
+            fi
+            set -e
+
+            # Construct the image name with a tag
+            ecr_image_name="${PRX_AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${safe_ecr_repo_name}:${PRX_COMMIT}"
+
+            # Export a variable whose name is the LABEL from the Dockerfile,
+            # and whose value is the full image name with tag
+            # e.g., if there's a LABEL org.prx.spire.publish.docker="WEB_SERVER"
+            # this would set WEB_SERVER=1234.dkr.ecr.us-eas-1.amazonaws.com...
+            declare -x "$label"="$ecr_image_name"
+
+            echo "> Pushing image $image_id to ECR $ecr_image_name"
+            docker tag $image_id $ecr_image_name
+            docker push $ecr_image_name
+            echo "< Finished publishing Docker image"
+        done
+    fi
+}
+
+# Look for any Docker images labeled with "org.prx.app"
+push_to_ecr_legacy() {
     echo ">>> Looking for publishable Docker images"
     image_id=$(docker images --filter "label=org.prx.app" --format "{{.ID}}" | head -n 1)
 
@@ -137,12 +188,14 @@ init() {
         then
             echo "> Publishing code"
             push_to_ecr
+            push_to_ecr_legacy
             push_to_s3_lambda
             push_to_s3_static
         elif [ "$PRX_CI_PRERELEASE" = "true" ]
         then
             echo "> Pushing pre-release code"
             push_to_ecr
+            push_to_ecr_legacy
             push_to_s3_lambda
             push_to_s3_static
         else
