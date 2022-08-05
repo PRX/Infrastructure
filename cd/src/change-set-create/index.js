5b9556dbe830c5aa66fa487e8d8e8881bbcb9700
Chris Kalafarski <chris@farski.com>
2022-08-05 08:06:32 -0400

    Wait for change set creation to complete

--- cd/src/change-set-create/index.js
+++ cd/src/change-set-create/index.js
@@ -121,6 +121,9 @@ exports.handler = async (event, context) => {
       ParameterValue: changeSetParams[k],
     }));
 
+    // createChangeSet returns immediately once the the request to make the
+    // change set has been made; it doesn't wait for the change set to actually
+    // get made.
     await cloudformation
       .createChangeSet({
         StackName: stackName,
@@ -140,7 +143,48 @@ exports.handler = async (event, context) => {
     fs.unlinkSync(configZipTempPath);
     fs.unlinkSync(repoZipTempPath);
 
-    await codepipeline.putJobSuccessResult({ jobId: job.id }).promise();
+    let changeSetDone = false;
+
+    do {
+      // Wait a few seconds
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+
+      // Check the creation status of the change set
+      const status = await cloudformation
+        .describeChangeSet({
+          StackName: stackName,
+          ChangeSetName: changeSetName,
+        })
+        .promise();
+
+      if (status.Status === 'CREATE_COMPLETE') {
+        // When it's created, the Lambda can stop running and the pipeline
+        // can continue
+        changeSetDone = true;
+        await codepipeline.putJobSuccessResult({ jobId: job.id }).promise();
+        break;
+      } else if (
+        // If it's still creating, the Lambda needs to keep waiting
+        ['CREATE_PENDING', 'CREATE_IN_PROGRESS'].includes(status.Status)
+      ) {
+        changeSetDone = false;
+        continue;
+      } else {
+        // Any other status would mean something went bad with the change set
+        // and the pipeline execution neesd to halt
+        changeSetDone = true;
+        await codepipeline
+          .putJobFailureResult({
+            jobId: job.id,
+            failureDetails: {
+              message: `Change set failed with ${status.Status}`,
+              type: 'JobFailed',
+            },
+          })
+          .promise();
+        break;
+      }
+    } while (changeSetDone === false);
   } catch (error) {
     console.error(error);
     await codepipeline
