e97aa5291de743ef81d4c2c90b84ad3222c3a626
Chris Kalafarski <chris@farski.com>
2022-08-18 19:50:56 -0400

    Use existing copy of root template

--- spire/cd/src/change-set-create/index.js
+++ spire/cd/src/change-set-create/index.js
@@ -3,41 +3,9 @@
  */
 
 const AWS = require('aws-sdk');
-const AdmZip = require('adm-zip');
-const fs = require('fs');
-const os = require('os');
-const path = require('path');
 
 const cloudformation = new AWS.CloudFormation({ apiVersion: '2010-05-15' });
 const codepipeline = new AWS.CodePipeline({ apiVersion: '2015-07-09' });
-const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
-
-/**
- * Downloads the given S3 object to a local file path
- * @param {string} bucketName
- * @param {string} objectKey
- * @param {string} filePath
- */
-function s3GetObject(bucketName, objectKey, filePath) {
-  return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(filePath);
-    const stream = s3
-      .getObject({
-        Bucket: bucketName,
-        Key: objectKey,
-      })
-      .createReadStream();
-
-    stream.on('error', reject);
-    file.on('error', reject);
-
-    file.on('finish', () => {
-      resolve(filePath);
-    });
-
-    stream.pipe(file);
-  });
-}
 
 /**
  * Publishes a Slack message to the relay SNS topic with information about a
@@ -46,43 +14,12 @@ function s3GetObject(bucketName, objectKey, filePath) {
  * @param {SNSEvent} event
  * @returns {Promise<void>}
  */
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   console.log(JSON.stringify(event));
 
   const job = event['CodePipeline.job'];
 
   try {
-    // Make a folder to hold local copies of the input artifacts
-    fs.mkdirSync(path.join(os.tmpdir(), context.awsRequestId));
-
-    const inputArtifacts = job.data.inputArtifacts;
-
-    // Download the Infrastructure repo Zip file, extract root.yml, and send
-    // it back to S3 so that the change set can reference it directly
-    const repoZipTempPath = path.join(
-      os.tmpdir(),
-      context.awsRequestId,
-      'repo.zip',
-    );
-    const repoArtifact = inputArtifacts.find(
-      (a) => a.name === 'InfrastructureRepoSourceArtifact',
-    );
-    await s3GetObject(
-      repoArtifact.location.s3Location.bucketName,
-      repoArtifact.location.s3Location.objectKey,
-      repoZipTempPath,
-    );
-    const repoZip = new AdmZip(repoZipTempPath);
-    const rootYaml = repoZip.readAsText('stacks/root.yml');
-    const rootYamlObjectKey = `${repoArtifact.location.s3Location.objectKey}/${context.awsRequestId}`;
-    await s3
-      .putObject({
-        Bucket: repoArtifact.location.s3Location.bucketName,
-        Key: rootYamlObjectKey,
-        Body: rootYaml,
-      })
-      .promise();
-
     const actionConfig = job.data.actionConfiguration;
     const userParamsJson = actionConfig.configuration.UserParameters;
     const userParams = JSON.parse(userParamsJson);
@@ -91,6 +28,7 @@ exports.handler = async (event, context) => {
     const changeSetName = userParams.ChangeSet;
     const roleArn = userParams.RoleArn;
     const paramOverrides = userParams.Parameters;
+    const templateUrlBase = userParams.TemplateUrlBase;
 
     const changeSetParams = paramOverrides;
     const changeSetParamsArray = Object.keys(changeSetParams).map((k) => ({
@@ -143,13 +81,11 @@ exports.handler = async (event, context) => {
           'CAPABILITY_NAMED_IAM',
           'CAPABILITY_AUTO_EXPAND',
         ],
-        TemplateURL: `https://s3.${process.env.AWS_REGION}.amazonaws.com/${repoArtifact.location.s3Location.bucketName}/${rootYamlObjectKey}`,
+        TemplateURL: `${templateUrlBase}/stacks/root.yml`,
         Parameters: changeSetParamsArray,
         IncludeNestedStacks: true,
       })
       .promise();
-
-    fs.unlinkSync(repoZipTempPath);
 
     let changeSetDone = false;
 
