7af0a712a94395335561e8edd3f1e8ab0b767e6a
Chris Kalafarski <chris@farski.com>
2023-04-10 16:01:30 -0400

    Upgrade DevOps Slack app runtime and SDK

--- devops/tooling/chat-ops/slack-app/lambda-src/app/access.js
+++ devops/tooling/chat-ops/slack-app/lambda-src/app/access.js
@@ -1,6 +1,10 @@
-const AWS = require('aws-sdk');
+/** @typedef {import('@aws-sdk/client-organizations').ListAccountsResponse} ListAccountsResponse */
+/** @typedef {import('@aws-sdk/client-sts').AssumeRoleResponse} AssumeRoleResponse */
 
-const sts = new AWS.STS({ apiVersion: '2011-06-15' });
+const { STS } = require('@aws-sdk/client-sts');
+const { Organizations } = require('@aws-sdk/client-organizations');
+
+const sts = new STS({ apiVersion: '2011-06-15' });
 
 module.exports = {
   regions() {
@@ -9,33 +13,29 @@ module.exports = {
   /**
    * Returns an assumed DevOps role from the given account
    * @param {string} awsAccountId
-   * @returns {Promise<AWS.STS.AssumeRoleResponse>}
+   * @returns {Promise<AssumeRoleResponse>}
    */
   async devopsRole(awsAccountId) {
     const roleArn = `arn:aws:iam::${awsAccountId}:role/${process.env.DEVOPS_CROSS_ACCOUNT_ACCESS_ROLE_NAME}`;
 
-    return sts
-      .assumeRole({
-        RoleArn: roleArn,
-        RoleSessionName: 'devops_slack_app',
-      })
-      .promise();
+    return sts.assumeRole({
+      RoleArn: roleArn,
+      RoleSessionName: 'devops_slack_app',
+    });
   },
   /**
    * Returns an assumed Organization data sharing role from the given account
-   * @returns {Promise<AWS.STS.AssumeRoleResponse>}
+   * @returns {Promise<AssumeRoleResponse>}
    */
   async orgSharingRole() {
-    return sts
-      .assumeRole({
-        RoleArn: process.env.AWS_ORGANIZATION_CROSS_ACCOUNT_SHARING_ROLE_ARN,
-        RoleSessionName: 'devops_slack_app',
-      })
-      .promise();
+    return sts.assumeRole({
+      RoleArn: process.env.AWS_ORGANIZATION_CROSS_ACCOUNT_SHARING_ROLE_ARN,
+      RoleSessionName: 'devops_slack_app',
+    });
   },
   /**
    * Returns a list of all AWS accounts that exist in an organization
-   * @returns {Promise<AWS.Organizations.ListAccountsResponse>}
+   * @returns {Promise<ListAccountsResponse>}
    */
   async orgAccounts() {
     // Assume a role within the Organization's management account that has
@@ -45,15 +45,17 @@ module.exports = {
     const role = await this.orgSharingRole();
 
     // The organizations endpoint only exists in us-east-1
-    const organizations = new AWS.Organizations({
+    const organizations = new Organizations({
       apiVersion: '2016-11-28',
       region: 'us-east-1',
-      accessKeyId: role.Credentials.AccessKeyId,
-      secretAccessKey: role.Credentials.SecretAccessKey,
-      sessionToken: role.Credentials.SessionToken,
+      credentials: {
+        accessKeyId: role.Credentials.AccessKeyId,
+        secretAccessKey: role.Credentials.SecretAccessKey,
+        sessionToken: role.Credentials.SessionToken,
+      },
     });
 
-    const accounts = await organizations.listAccounts({}).promise();
+    const accounts = await organizations.listAccounts({});
 
     return accounts;
   },
